d80ddf11679ce54bcd29ae1ca7e44f1412806d05
maasadi <m.a.asadi@live.com>
2018-10-05 17:22:15 -0400

    Add script

---
+++ machineInfo.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net"
+	"syscall"
+	"runtime"
+	"github.com/shirou/gopsutil/cpu"
+)
+
+const (
+	SYSINFO_LOADS_SCALE   =  1<<16; // 2^16 
+	SYSINFO_MEM_UNIT      =  1024 * 1024; // MB
+	PRINT_FLAG            =  0
+)
+
+// sys_t is go version of  "sysinfo" struct,
+// for Linux versions Since 2.3.23 (i386) and  2.3.48 (all architectures).
+// Reference: http://man7.org/linux/man-pages/man2/sysinfo.2.html
+type sys_t struct {
+	UpTime    int64      // Seconds since boot
+	Loads     [3]float64  // 1, 5, and 15 minute load averages
+	TotalRam  uint64     // Total usable main memory size
+	FreeRam   uint64     // Available memory size
+	SharedRam uint64     // Amount of shared memory
+	BufferRam uint64     // Memory used by buffers
+	TotalSwap uint64     // Total swap space size
+	FreeSwap  uint64     // Swap space still available
+	Procs     uint16     // Number of current processes
+	TotalHigh uint64     // Total high memory size
+	FreeHigh  uint64     // Available high memory size
+}
+
+// like gethosname() function in unistd.h, returns the hostname.
+func getHostName() string{
+
+	hostN, err := os.Hostname();
+	if (err != nil) {
+		panic(err);
+	}
+
+	// Print:
+	fmt.Printf("hostname:\t%v\n", hostN);
+
+	return hostN;
+}
+
+// return GOOS and GOARCH variables as strings.
+func getOSName() (string, string) {
+
+	// Print:
+	fmt.Printf("OS name:\t%v\n", runtime.GOOS);
+	fmt.Printf("OS arch:\t%v\n", runtime.GOARCH);
+
+	return runtime.GOOS, runtime.GOARCH;
+}
+
+// like sysinfo() in sys/sysinfo.h, returns system information as sys_t type.
+func getSysInfo () sys_t {
+	
+	sys_obj := sys_t{};
+	sys_ref := &syscall.Sysinfo_t{}
+	err := syscall.Sysinfo(sys_ref)
+	if  err != nil {
+		panic(err);
+	}
+	sys_obj.UpTime = sys_ref.Uptime;
+	sys_obj.Loads[0] = float64(sys_ref.Loads[0]) / SYSINFO_LOADS_SCALE;
+	sys_obj.Loads[1] = float64(sys_ref.Loads[1]) / SYSINFO_LOADS_SCALE;
+	sys_obj.Loads[2] = float64(sys_ref.Loads[2]) / SYSINFO_LOADS_SCALE;
+	sys_obj.TotalRam = sys_ref.Totalram / SYSINFO_MEM_UNIT;
+	sys_obj.FreeRam = sys_ref.Freeram / SYSINFO_MEM_UNIT;
+	sys_obj.SharedRam = sys_ref.Sharedram / SYSINFO_MEM_UNIT;
+	sys_obj.BufferRam = sys_ref.Bufferram / SYSINFO_MEM_UNIT;
+	sys_obj.TotalSwap = sys_ref.Totalswap / SYSINFO_MEM_UNIT;	
+	sys_obj.FreeSwap = sys_ref.Freeswap / SYSINFO_MEM_UNIT;
+	sys_obj.Procs = sys_ref.Procs;	
+	sys_obj.TotalHigh = sys_ref.Totalhigh / SYSINFO_MEM_UNIT;
+	sys_obj.FreeHigh = sys_ref.Freehigh / SYSINFO_MEM_UNIT;
+
+	// Print:
+	fmt.Printf("sys uptime:\t%d\n", sys_obj.UpTime);
+	fmt.Printf("sys load avg:\t%2.2f, %2.2f, %2.2f\n",
+		sys_obj.Loads[0],sys_obj.Loads[1],sys_obj.Loads[2]);
+	fmt.Printf("sys totalRam:\t%d MB\n", sys_obj.TotalRam);
+	fmt.Printf("sys freeRam:\t%d MB\n", sys_obj.TotalRam);
+	fmt.Printf("sys sharedRam:\t%d MB\n", sys_obj.SharedRam);
+	fmt.Printf("sys bufferRam:\t%d MB\n", sys_obj.BufferRam);
+	fmt.Printf("sys totalSwap:\t%d MB\n", sys_obj.TotalSwap);
+	fmt.Printf("sys freeSwap:\t%d MB\n", sys_obj.FreeSwap);
+	fmt.Printf("sys totalHigh:\t%d MB\n", sys_obj.TotalHigh);
+	fmt.Printf("sys freeHigh:\t%d MB\n", sys_obj.FreeHigh);
+	fmt.Printf("sys procs:\t%d\n", sys_obj.Procs);
+
+	return sys_obj;
+}
+
+// like sysinfo in sys/sysinfo.h, returns the cpu information in /proc/cpuinfo
+// Reference: https://github.com/shirou/gopsutil
+func getCPUInfo () []cpu.InfoStat {
+
+	var cpu_info []cpu.InfoStat;
+	cpu_info, err := cpu.Info()
+	if err != nil {
+		panic (err);
+	}
+
+	// Print:
+	sz := 1;
+	if PRINT_FLAG != 0 {
+		sz = len(cpu_info);
+	}
+	
+	for i := 0; i < sz; i++ {
+		fmt.Printf("cpuID:\t%d\n", cpu_info[i].CPU);
+		fmt.Printf("--vendorID:\t%s\n",cpu_info[i].VendorID);
+		fmt.Printf("--family:\t%s\n", cpu_info[i].Family);
+		fmt.Printf("--model:\t%s\n", cpu_info[i].Model);
+		fmt.Printf("--stepping:\t%d\n", cpu_info[i].Stepping);
+		fmt.Printf("--physicalID:\t%s\n", cpu_info[i].PhysicalID);
+		fmt.Printf("--coreID:\t%s\n", cpu_info[i].CoreID);
+		fmt.Printf("--cores:\t%d\n", cpu_info[i].Cores);
+		fmt.Printf("--modelName:\t%s\n", cpu_info[i].ModelName);
+		fmt.Printf("--MHz:\t\t%g\n", cpu_info[i].Mhz);
+		fmt.Printf("--facheSize:\t%d\n", cpu_info[i].CacheSize);
+		fmt.Printf("--flags:\t%v\n", cpu_info[i].Flags);
+		fmt.Printf("--Microcode:\t%s\n", cpu_info[i].Microcode);
+	}
+
+	return cpu_info;
+}
+
+// like ifconfig, returns the network interfaces of /sys/class/net
+// Reference:  https://golang.org/pkg/net/
+func getNetworkInterface() []net.Interface{
+
+	var iface []net.Interface;
+	iface, err := net.Interfaces();
+	if err != nil {
+		panic(err);
+	}
+
+	// Print:
+	fmt.Printf("interfaces:\t%v\n", iface[0]);
+	for i := 1; i < len(iface); i++ {
+		fmt.Printf("\t\t%v\n", iface[i]);
+	}
+
+	return iface;
+}
+
+func main () {
+	getHostName();
+	getNetworkInterface();
+	getOSName();
+	getSysInfo();
+	getCPUInfo();
+}
