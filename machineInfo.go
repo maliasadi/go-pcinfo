fc3b2774a921366ce286cbe5bf871174d3c76e6d
maa277 <m.a.asadi@live.com>
2018-11-01 00:46:53 -0400

    Update machineInfo.go

--- machineInfo.go
+++ machineInfo.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"net"
-	"syscall"
-	"runtime"
+	"os"       // provides a platform-independent interface to operating system functionality
+	"fmt"      // implements formatted I/O with functions analogous to C's printf and scanf.
+	"net"      // provides a portable interface for network I/O, including TCP/IP, UDP, domain name resolution, and Unix domain sockets.
+	"flag"     // implements command-line flag parsing.
+	"syscall"  // contains an interface to the low-level operating system primitives.
+	"runtime"  // contains operations that interact with Go's runtime system, such as functions to control goroutines.
 	"github.com/shirou/gopsutil/cpu"
 )
 
 const (
 	SYSINFO_LOADS_SCALE   =  1<<16; // 2^16 
 	SYSINFO_MEM_UNIT      =  1024 * 1024; // MB
-	PRINT_FLAG            =  1 // complete (or partial) print
 )
 
 // sys_t is go version of  "sysinfo" struct,
@@ -39,25 +39,17 @@ func GetHostName() string{
 	if (err != nil) {
 		panic(err);
 	}
-
-	// Print:
-	fmt.Printf("hostname:\t%v\n", hostN);
-
+	
 	return hostN;
 }
 
 // return GOOS and GOARCH variables as strings.
 func GetOSName() (string, string) {
-
-	// Print:
-	fmt.Printf("OS name:\t%v\n", runtime.GOOS);
-	fmt.Printf("OS arch:\t%v\n", runtime.GOARCH);
-
 	return runtime.GOOS, runtime.GOARCH;
 }
 
 // like sysinfo() in sys/sysinfo.h, returns system information as sys_t type.
-func GetSysInfo () sys_t {
+func GetSysInfo() sys_t {
 	
 	sys_obj := sys_t{};
 	sys_ref := &syscall.Sysinfo_t{}
@@ -79,7 +71,49 @@ func GetSysInfo () sys_t {
 	sys_obj.TotalHigh = sys_ref.Totalhigh / SYSINFO_MEM_UNIT;
 	sys_obj.FreeHigh = sys_ref.Freehigh / SYSINFO_MEM_UNIT;
 
-	// Print:
+	return sys_obj;
+}
+
+// like sysinfo in sys/sysinfo.h, returns the cpu information in /proc/cpuinfo
+// NOTE: if PRINT_FLAG != 0 then print all CPUs info. 
+// Reference: https://github.com/shirou/gopsutil
+func GetCPUInfo() []cpu.InfoStat {
+
+	var cpu_info []cpu.InfoStat;
+	cpu_info, err := cpu.Info()
+	if err != nil {
+		panic (err);
+	}
+
+	return cpu_info;
+}
+
+// like ifconfig, returns the network interfaces of /sys/class/net
+// Reference:  https://golang.org/pkg/net/
+func GetNetworkInterface() []net.Interface{
+
+	var ifaces []net.Interface;
+	ifaces, err := net.Interfaces();
+	if err != nil {
+		panic(err);
+	}
+	
+	return ifaces;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+func PrintHostName() {	
+	fmt.Printf("hostname:\t%v\n", GetHostName());
+}
+
+func PrintOSName() {
+	fmt.Printf("OS name:\t%v\n", runtime.GOOS);
+	fmt.Printf("OS arch:\t%v\n", runtime.GOARCH);
+}
+
+func PrintSysInfo() {
+	sys_obj := GetSysInfo();
 	fmt.Printf("sys uptime:\t%d\n", sys_obj.UpTime);
 	fmt.Printf("sys load avg:\t%2.2f, %2.2f, %2.2f\n",
 		sys_obj.Loads[0],sys_obj.Loads[1],sys_obj.Loads[2]);
@@ -92,28 +126,11 @@ func GetSysInfo () sys_t {
 	fmt.Printf("sys totalHigh:\t%d MB\n", sys_obj.TotalHigh);
 	fmt.Printf("sys freeHigh:\t%d MB\n", sys_obj.FreeHigh);
 	fmt.Printf("sys procs:\t%d\n", sys_obj.Procs);
-
-	return sys_obj;
 }
 
-// like sysinfo in sys/sysinfo.h, returns the cpu information in /proc/cpuinfo
-// NOTE: if PRINT_FLAG != 0 then print all CPUs info. 
-// Reference: https://github.com/shirou/gopsutil
-func GetCPUInfo () []cpu.InfoStat {
-
-	var cpu_info []cpu.InfoStat;
-	cpu_info, err := cpu.Info()
-	if err != nil {
-		panic (err);
-	}
-
-	// Print:
-	sz := 1;
-	if PRINT_FLAG != 0 {
-		sz = len(cpu_info);
-	}
-	
-	for i := 0; i < sz; i++ {
+func PrintCPUInfo() {
+	cpu_info := GetCPUInfo();	
+	for i := 0; i < len(cpu_info); i++ {
 		fmt.Printf("cpuID:\t%d\n", cpu_info[i].CPU);
 		fmt.Printf("--vendorID:\t%s\n",cpu_info[i].VendorID);
 		fmt.Printf("--family:\t%s\n", cpu_info[i].Family);
@@ -128,33 +145,37 @@ func GetCPUInfo () []cpu.InfoStat {
 		fmt.Printf("--flags:\t%v\n", cpu_info[i].Flags);
 		fmt.Printf("--Microcode:\t%s\n", cpu_info[i].Microcode);
 	}
-
-	return cpu_info;
 }
 
-// like ifconfig, returns the network interfaces of /sys/class/net
-// Reference:  https://golang.org/pkg/net/
-func GetNetworkInterface() []net.Interface{
-
-	var iface []net.Interface;
-	iface, err := net.Interfaces();
-	if err != nil {
-		panic(err);
+func PrintNetworkInterface() {
+	ifaces := GetNetworkInterface();
+	fmt.Printf("interfaces:\t%v\n", ifaces[0]);
+	for i := 1; i < len(ifaces); i++ {
+		fmt.Printf("\t\t%v\n", ifaces[i]);
 	}
-
-	// Print:
-	fmt.Printf("interfaces:\t%v\n", iface[0]);
-	for i := 1; i < len(iface); i++ {
-		fmt.Printf("\t\t%v\n", iface[i]);
-	}
-
-	return iface;
 }
+
 
 func main () {
-	GetHostName();
-	GetNetworkInterface();
-	GetOSName();
-	GetSysInfo();
-	GetCPUInfo();
+
+	var hn = flag.Bool("hostname", false, "if 'hostname=true' => print hostname");
+	var OS = flag.Bool("os", false, "if 'os=true' => print operating system info");
+	var cpu = flag.Bool("cpu", false, "if 'cpu=true' => print cpu info");
+	var netw = flag.Bool("network", false,"if 'network=true' => print network interfaces info");
+
+	flag.Parse()
+	fmt.Println (len(os.Args));
+	if len(os.Args) == 1 {
+		PrintHostName();
+		PrintNetworkInterface();
+		PrintOSName();
+		PrintSysInfo();
+		PrintCPUInfo();
+		return;
+	}
+
+	if *hn { PrintHostName(); }
+	if *OS { PrintSysInfo(); PrintCPUInfo(); }
+	if *cpu { PrintCPUInfo(); }
+	if *netw { PrintNetworkInterface(); }
 }
